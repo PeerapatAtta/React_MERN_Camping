89a2cfc1bfbba3d42d6d172e6b1b4f7fdf5b98f1
OFF <peerapat13th@gmail.com>
2025-07-17 00:42:55 +0700

    19. Clerk Token

--- client/src/utils/schemas.jsx
+++ client/src/utils/schemas.jsx
@@ -1,7 +1,7 @@
 import {z} from 'zod';
 
 // Define the schema for validation
-const campingSchema = z.object({
+export const campingSchema = z.object({
     title: z.string().min(2, 'Title must be at least 2 characters long'),
     price: z.coerce.number().min(0, 'Price must be a positive number'),
     description: z.string().max(50, 'Description must be less than 50 characters'),
@@ -10,4 +10,7 @@ const campingSchema = z.object({
     lng: z.coerce.number().optional(),
 })
 
-export default campingSchema
+export const profileSchema = z.object({
+    firstname: z.string().min(2, 'Firstname must be at least 2 characters long'),
+    lastname: z.string().min(2, 'Lastname must be at least 2 characters long'),
+})
