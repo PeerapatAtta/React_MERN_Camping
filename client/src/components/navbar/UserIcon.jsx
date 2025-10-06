030f64b3fafac0db8ffc97ce7bb83d934f9d47f9
OFF <peerapat13th@gmail.com>
2025-10-07 01:04:19 +0700

    53. Profile Image

--- client/src/components/navbar/UserIcon.jsx
+++ client/src/components/navbar/UserIcon.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import { CircleUserRound } from 'lucide-react';
+import { useUser } from '@clerk/clerk-react';
 
 const UserIcon = () => {
+  const { user } = useUser();
+  // console.log(user);
+
+  if (user) {
+    return (
+      <img
+        src={user.imageUrl}
+        className='w-6 h-6 rounded-full object-cover'
+      />
+    )
+  }
+
   return (
     <CircleUserRound />
   )
